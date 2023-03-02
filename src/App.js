f9f56365679c6ed036058d3860e28dff407ac76e
Wolrang <dkfud252@naver.com>
2023-03-02 12:45:05 +0900

    🎉 React Basic

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let user = 'World';
+  // jsx문법에서는 전체를 감싸는 태그 안에 다른 태그들을 만들 수 있음 
+  // jsx문법에서는 클로징 태그 필수
   return (
     <div className="App">
-      <h1>React</h1>
+      <h1>Hello, {user}!</h1>
+      <p>This is a React App</p>
     </div>
   );
 }
 
-export default App;
+export default App; 4
