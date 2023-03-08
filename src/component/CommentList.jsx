7197cea5b71a57ee278eeb7ed57680b5c59c98f6
Wolrang <dkfud252@naver.com>
2023-03-08 17:05:49 +0900

    🎉 React Basic

--- src/component/CommentList.jsx
+++ src/component/CommentList.jsx
@@ -106,4 +106,47 @@ function CommentList() {
   )
 }
 
+// 선생님꺼
+// function CommentList() {
+//   const [commentList, setCommentList] = useState(comments);
+//   const [name, setName] = useState('');
+//   const [content, setContent] = useState('');
+
+//   const deleteComment = (index) => {
+//     const newCommentList = [...commentList];
+//     newCommentList.splice(index, 1);
+//     setCommentList(newCommentList)
+//     console.log(index)
+//   }
+
+//   const addComment = () => {
+//     // const add = {name: "히히ㅇㅎㅇㄶ히", comment : "아아아아아?~!!!"}
+//     // setCommentList([add,...commentList])
+
+//     const add = {name : name, comment: content}
+//     setCommentList([add,...commentList])
+//     // state변경 함수에 빈 문자열을 넣어서 input에 작성된 내용을 초기화
+//     setName('')
+//     setContent('')
+//   }
+
+//   return (
+//     <div>
+//       {
+//         commentList.map((comment, i)=> {
+//           return (
+//             <Comment name={comment.name} comment={comment.comment} key={i} onDelete={()=> deleteComment(i)}/>
+//           )
+//         })
+//       }
+//       {/* onChange 이벤트는 input 요소에서 값이 변경될 때 발생 
+//           그 때 발생한 이벤트를 매개변수 e
+//       */}
+//       이름 : <input value={name} onChange={(e) => setName(e.target.value)}/>
+//       댓글 : <input value={content} onChange={(e) => setContent(e.target.value)}/>
+//       <button onClick={addComment}>글추가</button>
+//     </div>
+//   )
+// }
+
 export default CommentList;
